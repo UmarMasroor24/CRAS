0ea907173e59b96f8222ac62867a8682107b3883
UmarMasroor24 <ummermasroor24@gmail.com>
2020-08-04 13:56:43 +0500

    third commit

--- cmd.py
+++ cmd.py
@@ -1,5 +1,9 @@
 import os
 os.system('cmd /c "git checkout master"')
 os.system('cmd /c "git status"')
-os.system('cmd /c "git add ."')
-os.system('cmd /c "git commit -m "Auto Update""')
+os.system('cmd /c "git add GBOOST"')
+os.system('cmd /c "git add Prophet"')
+os.system('cmd /c "git add RandomForest"')
+os.system('cmd /c "git add Lasso"')
+os.system('cmd /c "git add Arima"')
+os.system('cmd /c "git commit -m "Update Models in git REPO""')
