86b227faf7b2d12f05e250ce1e71a0d9ff8839c4
UmarMasroor24 <ummermasroor24@gmail.com>
2020-08-04 10:34:11 +0500

    Auto Update

--- cmd.py
+++ cmd.py
@@ -1,4 +1,5 @@
 import os
+os.system('cmd /c "git checkout master"')
 os.system('cmd /c "git status"')
 os.system('cmd /c "git add ."')
-os.system('cmd /c "git com"')
+os.system('cmd /c "git commit -m "Auto Update""')
